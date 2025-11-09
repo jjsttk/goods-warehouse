ade784511be84a87799c72a873ea8fc4e07f8adc
Aleksandr Konstantinov <kraizlol@Mail.ru>
2025-11-09 21:13:52 +0400

    Fix convert method (page(exchangeResponse) -> customPage(controllerResponse)) (#8)

--- src/main/java/com/jjsttk/goodswarehouse/mapper/product/MapstructProductMapper.java
+++ src/main/java/com/jjsttk/goodswarehouse/mapper/product/MapstructProductMapper.java
@@ -3,11 +3,14 @@ package com.jjsttk.goodswarehouse.mapper.product;
 import com.jjsttk.goodswarehouse.controller.product.dto.response.PageGetProductResponse;
 import com.jjsttk.goodswarehouse.controller.product.dto.response.GetProductResponse;
 import com.jjsttk.goodswarehouse.service.product.dto.response.BaseProductServiceDto;
+import com.jjsttk.goodswarehouse.service.product.price.exchange.dto.response.ProductPriceExchangeServiceResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.data.domain.Page;
 
 /**
  * MapStruct mapper for converting {@link BaseProductServiceDto} objects
@@ -26,4 +29,13 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class MapstructProductMapper implements ProductConverter {
+
+    @Override
+    @Mapping(source = "totalElements", target = "totalCount")
+    @Mapping(source = "number", target = "currentPage")
+    @Mapping(source = "size", target = "pageSize")
+    @Mapping(source = "numberOfElements", target = "currentPageSize")
+    public abstract PageGetProductResponse<GetProductResponse> mapToControllerResponse(
+            Page<ProductPriceExchangeServiceResponse> serviceResponse
+    );
 }
